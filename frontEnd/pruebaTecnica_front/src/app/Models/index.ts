ebe117cd46f10218139dde3c8d111286adb44c7f
EmanuelPe01 <emanuel03romo@gmail.com>
2025-06-26 21:06:37 -0600

    feat: Se configura construccion productiva de angular y se configura laravel para servir front de angular

--- frontEnd/pruebaTecnica_front/src/app/Models/index.ts
+++ frontEnd/pruebaTecnica_front/src/app/Models/index.ts
@@ -1,2 +1,2 @@
 export {ApiResponse, DetalleRollos, RolloTela, TipoTela, ColorTela, DetalleColores, DetalleTipos, Venta} from './ModelResponseTelas'
-export const endpoint: string = 'http://localhost:8000/api/'
+export const endpoint: string = '/api/'
