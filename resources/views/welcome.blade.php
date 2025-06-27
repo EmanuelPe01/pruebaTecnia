bfe8d22abd8a4ca2ad3bade15e99aaaec9ffa626
EmanuelPe01 <emanuel03romo@gmail.com>
2025-06-26 21:08:11 -0600

    fix (resources/views/welcome.blade.php): Se modifica ruta base de la app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Mi Sistema de Telas</title>
-    <base href="/angular/">
+    <base href="/app/">
 
     {{-- CSS compilado por Angular --}}
     <link rel="stylesheet" href="{{ asset('angular/styles.css') }}">
